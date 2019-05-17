04c70a15d29998428a0c65d032982170a3b5fc91
github username <github email address>
2019-05-17 16:30:30 -0400

    updates file tree and list_events method to match new site css

--- lib/jersey_family_fun/cli.rb
+++ lib/jersey_family_fun/cli.rb
@@ -1,11 +1,11 @@
 class JerseyFamilyFun::CLI
-  
+
   def call
     list_events
     menu
     goodbye
   end
-  
+
   def list_events
     puts "Welcome to Jersey Family Fun! Here is a list of today's events:"
     @events = JerseyFamilyFun::Event.today
@@ -13,13 +13,13 @@ class JerseyFamilyFun::CLI
       puts "#{index}. #{event.title_and_location} - #{event.date_and_time} - #{event.url}"
     end
   end
-  
+
   def menu
     input = nil
     while input != 'exit'
       puts "Enter the number of the event to go to that event's webpage, or 'list' to see today's list of events, or 'exit':"
       input = gets.strip.downcase
-      
+
       if input.to_i > 0 && input.to_i <= @events.size
         system("open #{@events[input.to_i - 1].url}")
       elsif input == "list"
@@ -29,9 +29,9 @@ class JerseyFamilyFun::CLI
       end
     end
   end
-  
+
   def goodbye
     puts "Come back tomorrow for more family fun events!"
-  end 
+  end
 
 end
--- lib/jersey_family_fun/event.rb
+++ lib/jersey_family_fun/event.rb
@@ -1,21 +1,20 @@
 class JerseyFamilyFun::Event
-  
+
   attr_accessor :title_and_location, :date_and_time, :url
-  
+
   def self.today
     self.scrape_events
   end
-  
+
   def self.scrape_events
     doc = Nokogiri::HTML(open("https://www.njkidsonline.com/events"))
-    doc.css("div.ListingItem").collect do |listed_event|
+    doc.css('div.col-12').collect do |listed_event|
       event = self.new
       event.title_and_location = listed_event.css("h3").text.strip
       event.date_and_time = listed_event.css(".FieldRow").text.gsub("Date", "").strip
-      event.url = "www.njkidsonline.com#{listed_event.css("a").attribute('href').value}"
-      #binding.pry
+      event.url = "www.njkidsonline.com#{listed_event.css('a').attribute('href')}"
       event
     end
   end
-  
+
 end
